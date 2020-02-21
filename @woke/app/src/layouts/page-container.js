edb7414b056cf957a8a07a486ce6c3d9703178f4
bill <manbil03@gmail.com>
2020-02-22 04:37:27 +1030

    Issue #37: Untested attempt to fix desktop responsivse style issues

--- @woke/app/src/layouts/page-container.js
+++ @woke/app/src/layouts/page-container.js
@@ -11,8 +11,7 @@ import { makeStyles, useTheme} from '@material-ui/styles';
 const useStyles = makeStyles(theme => ({
 	root: {
 		background: theme.background,
-		height: theme.breakpoints.values.xl,
-		width: theme.breakpoints.values.sm,
+		height: '100vh',
 	},
 
 	container: {
@@ -30,7 +29,7 @@ export default function PageContainer(props) {
 	return (
 		<React.Fragment>
 			<CssBaseline />
-			<Container maxWidth="sm" className={styles.container}>
+			<Container className={styles.container}>
 				<Box component="div" position="relative" className={styles.root}>
 					{props.children}
 				</Box>
