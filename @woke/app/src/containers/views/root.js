39a2034eb441f553b640366d4a308eb921f98b8d
bill <manbil03@gmail.com>
2020-04-11 09:16:55 +0930

    app:feature:implement routing for /how page and future pages

--- @woke/app/src/containers/views/root.js
+++ @woke/app/src/containers/views/root.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
+//Router
+import { BrowserRouter as Switch, Route } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import How from '../../layouts/how/index';
+
 // Layout
 import Theming from '../../layouts/theming'
 import RootContainer from '../../layouts/container-root'
@@ -8,6 +14,8 @@ import NavBar from '../../components/navbar'
 // Hooks
 // @TODO views should not contain app state
 import { useRootContext } from '../../hooks/root-context'
+
+const history = createBrowserHistory();
 
 export default function RootView({children}) {
 	const { loading, headerChildren } = useRootContext();
@@ -21,7 +29,12 @@ export default function RootView({children}) {
 	return (
 		<Theming>
 			<RootContainer NavBar={makeNavBar()} headerChildren={headerChildren}>
-				{children}
+				<Router history={history}>
+					<Switch>
+						<Route exact path='/'>{children}</Route>
+						<Route exact path='/How' component={How} />
+					</Switch>
+				</Router>
 			</RootContainer>
 		</Theming>
 	);
---
+++ @woke/app/src/layouts/how/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import FlexRow from '../../layouts/flex-row';
+
+//Placeholder how page layout
+
+export default function How () {
+    
+	return (
+		<FlexRow>
+            <Typography variant='h1'>
+                how the heck
+            </Typography>
+		</FlexRow>
+	);
+}
