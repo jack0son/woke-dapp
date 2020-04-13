e87d8dc4cc50d1dd1989867c671810035cbfed04
bill <manbil03@gmail.com>
2020-04-13 15:47:16 +0930

    app:design:regression repair increase suggestion box z-index

--- @woke/app/src/components/typeahead.js
+++ @woke/app/src/components/typeahead.js
@@ -93,7 +93,7 @@ const useStyles = makeStyles(theme => ({
   },
   paper: {
     position: 'absolute',
-    zIndex: 1,
+    zIndex: 2,
 		//marginTop: theme.spacing(1),
 		height: theme.spacing(4),
 		minHeight: theme.spacing(4),
