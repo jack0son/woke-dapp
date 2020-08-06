72bf109b63acf26225688a098ce0f4be84b1ceda
Jack <jackson.virgo1@gmail.com>
2020-08-06 13:49:05 +0930

    Add prettier-eslint config (coc-prettier)

--- @woke/wact/src/actor-system.js
+++ @woke/wact/src/actor-system.js
@@ -18,9 +18,8 @@ const MessageDebugger = require('./lib/message-debugger');
 // the message bundle.
 // @param receivers fn
 // @returns Map string -> fn
-const bind_receivers = (receivers, msg, state, ctx) => receivers ?
-	receivers({ msg, state, ctx })
-	: undefined;
+const bind_receivers = (receivers, msg, state, ctx) =>
+	receivers ? receivers({ msg, state, ctx }) : undefined;
 
 // Spawn a stateful actor
 const spawn_actor = (_parent, _name, _actionsMap, _initialState, _properties) =>
@@ -31,15 +30,21 @@ const spawn_actor = (_parent, _name, _actionsMap, _initialState, _properties) =>
 			return route_action(_actionsMap, state, msg, {
 				...context,
 				receivers: bind_receivers(_properties.receivers, msg, state, context),
-			})
+			});
 		},
 		_name,
-		_properties,
+		_properties
 	);
 
 // Spawn an actor which can persist its state to a storage repository
-const spawn_persistent = (_parent, _name, _actionsMap, _initialState, _properties) => {
-	if(!_properties || !_properties.persistenceKey) {
+const spawn_persistent = (
+	_parent,
+	_name,
+	_actionsMap,
+	_initialState,
+	_properties
+) => {
+	if (!_properties || !_properties.persistenceKey) {
 		throw new Error(`Persistent actor must define 'persistenceKey' property`);
 	}
 	const { persistenceKey, ...otherProperties } = _properties;
@@ -48,76 +53,69 @@ const spawn_persistent = (_parent, _name, _actionsMap, _initialState, _propertie
 	debug.control.enabledByApp = debug.control.enabled();
 
 	let recovering = false;
-	const target = debug.control.enabledByApp ? 
-		(state = _initialState, msg, context) => {
-			context = {...context, debug }
-			if(context.recovering) {
-				if(!recovering) {
-					recovering = true;
-					debug.log(`----- Recovering persisted state...`);
-					if(!DEBUG_RECOVERY) {
-						debug.control.disable();
+	const target = debug.control.enabledByApp
+		? (state = _initialState, msg, context) => {
+				context = { ...context, debug };
+				if (context.recovering) {
+					if (!recovering) {
+						recovering = true;
+						debug.log(`----- Recovering persisted state...`);
+						if (!DEBUG_RECOVERY) {
+							debug.control.disable();
+						}
 					}
+				} else if (recovering) {
+					recovering = false;
+					if (!debug.control.enabled()) {
+						debug.control.enable();
+					}
+					debug.log(`----- ... recovery complete.`);
 				}
-			} else if(recovering) {
-				recovering = false;
-				if(!debug.control.enabled()) {
-					debug.control.enable();
-				}
-				debug.log(`----- ... recovery complete.`);
-			}
 
-			return route_action(_actionsMap, state, msg, {
-				...context,
-				receivers: bind_receivers(_properties.receivers, msg, state, context),
-			})
-		}
+				return route_action(_actionsMap, state, msg, {
+					...context,
+					receivers: bind_receivers(_properties.receivers, msg, state, context),
+				});
+		  }
 		: (state = _initialState, msg, context) => {
-			context = {...context, debug }
-			return route_action(_actionsMap, state, msg, {
-				...context,
-				receivers: bind_receivers(_properties.receivers, msg, state, context),
-			})
-		};
+				context = { ...context, debug };
+				return route_action(_actionsMap, state, msg, {
+					...context,
+					receivers: bind_receivers(_properties.receivers, msg, state, context),
+				});
+		  };
 
-	return spawnPersistent(
-		_parent,
-		target,
-		persistenceKey,
-		_name,
-		_properties,
-	);
-}
+	return spawnPersistent(_parent, target, persistenceKey, _name, _properties);
+};
 
 // Pass message bundle to action handler
 // @returns next actor state
 const route_action = async (_actionsMap, _state, _msg, _context) => {
 	let action = _actionsMap[_msg.type];
-	if(action && typeof (action) == "function") {
+	if (action && typeof action == 'function') {
 		const nextState = await action(_msg, _context, _state);
 		return nextState !== undefined ? nextState : _state;
-	}
-	else {
+	} else {
 		console.warn(`${_context.name} ignored unknown message:`, _msg);
 		return _state;
 	}
-}
+};
 
-const isSystem = _system => !!_system && !!_system.name;
+const isSystem = (_system) => !!_system && !!_system.name;
 
 // @TODO define persistent properties
-const isPersistentSystem = _system => isSystem(_system);
+const isPersistentSystem = (_system) => isSystem(_system);
 
 // Spawn an actor instance using an actor definition
 // @returns actor instance
 function start_actor(_parent) {
 	return (_name, _definition, _initialState) => {
-		if(!isSystem(_parent)) {
+		if (!isSystem(_parent)) {
 			throw new Error(`Parent actor must be provided`);
 		}
 		const { actions, properties } = _definition;
-		const { initialState, ...otherProperties} = properties;
-		if(!actions) {
+		const { initialState, ...otherProperties } = properties;
+		if (!actions) {
 			throw new Error(`No actions defined for '${_name}' actor`);
 		}
 
@@ -125,21 +123,25 @@ function start_actor(_parent) {
 			_parent,
 			_name,
 			actions,
-			{...(initialState ? initialState : {}), ..._initialState},
-			otherProperties,
+			{ ...(initialState ? initialState : {}), ..._initialState },
+			otherProperties
 		);
-	}
+	};
 }
 
 // Spawn a persistent actor
 // @returns persistant actor instance
-const start_persistent = _persistentSystem => (_name, _definition, _initialState) => {
-	if(!isPersistentSystem(_persistentSystem)) {
+const start_persistent = (_persistentSystem) => (
+	_name,
+	_definition,
+	_initialState
+) => {
+	if (!isPersistentSystem(_persistentSystem)) {
 		throw new Error(`Persistent system must be provided`);
 	}
 	const { actions, properties } = _definition;
-	const { initialState, ...otherProperties} = properties;
-	if(!actions) {
+	const { initialState, ...otherProperties } = properties;
+	if (!actions) {
 		throw new Error(`No actions defined for {${_name}} actor`);
 	}
 
@@ -147,21 +149,23 @@ const start_persistent = _persistentSystem => (_name, _definition, _initialState
 		_persistentSystem,
 		_name,
 		actions,
-		{...(initialState ? initialState : {}), ..._initialState},
-		otherProperties,
+		{ ...(initialState ? initialState : {}), ..._initialState },
+		otherProperties
 	);
-}
+};
 
 // Instantiate a nact actor system
 // @returns director: nact actor system with bound methods
 function bootstrap(_persistenceEngine) {
-	const system = _persistenceEngine  ? start(configurePersistence(_persistenceEngine)) : start();
+	const system = _persistenceEngine
+		? start(configurePersistence(_persistenceEngine))
+		: start();
 	return {
 		start_actor: start_actor(system),
 		start_persistent: _persistenceEngine ? start_persistent(system) : undefined,
 		stop: () => stop(system),
 		system,
-	}
+	};
 }
 
 module.exports = {
@@ -173,4 +177,4 @@ module.exports = {
 	dispatch,
 	query,
 	block,
-}
+};
