0735333746b6df8c766bf4c8a29a02e3be22e076
Jack <jackson.virgo1@gmail.com>
2020-08-11 19:41:44 +0930

    web3-nact:fix: on chain error definition

--- @woke/web3-nact/src/lib/errors.js
+++ @woke/web3-nact/src/lib/errors.js
@@ -7,14 +7,16 @@ class DomainError extends Error {
 }
 
 class ProviderError extends DomainError {
-	constructor(error, network) { // and tx data?
+	constructor(error, network) {
+		// and tx data?
 		super(`FATAL: Connection to provider '${network.id}:${network.name}' failed`);
 		this.network = network;
 	}
 }
 
 class ParamError extends DomainError {
-	constructor(error, tx) { // and tx data?
+	constructor(error, tx) {
+		// and tx data?
 		super(`Transaction '${tx.method}' failed due to invalid parameters.`);
 		this.web3Error = error;
 		this.data = { tx };
@@ -22,7 +24,8 @@ class ParamError extends DomainError {
 }
 
 class TransactionError extends DomainError {
-	constructor(error, tx) { // and tx data?
+	constructor(error, tx) {
+		// and tx data?
 		super(`Transaction '${tx.method}' failed.`);
 		this.web3Error = error;
 		this.data = { tx };
@@ -30,19 +33,24 @@ class TransactionError extends DomainError {
 }
 
 class OnChainError extends DomainError {
-	constructor(error, tx, receipt) { // and tx data?
+	constructor(error, tx, receipt) {
+		// and tx data?
 		super(`Transactions ${receipt.transactionHash} failed on chain.`);
 		this.web3Error = error;
-		this.data = { receip, tx };
+		this.data = { receipt, tx };
 	}
 }
 
 class RevertError extends DomainError {
 	constructor(error, tx) {
-		super(`Transaction reverted ${error.reason ? `'${error.reason}'` : ` with no reason provided`}`);
+		super(
+			`Transaction reverted ${
+				error.reason ? `'${error.reason}'` : ` with no reason provided`
+			}`
+		);
 		this.web3Error = error;
 		this.reason = error.reason;
-		this.data = { tx }
+		this.data = { tx };
 	}
 }
 
@@ -53,4 +61,4 @@ module.exports = {
 	TransactionError,
 	OnChainError,
 	RevertError,
-}
+};
