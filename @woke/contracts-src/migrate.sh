03bd3c86c52c829d9fd0e69f03190e17f24f9ef5
Jack <jackson.virgo1@gmail.com>
2020-09-22 09:18:59 +0930

    contracts: Migration script clones artifacts between repo duplicates

--- @woke/contracts-src/migrate.sh
+++ @woke/contracts-src/migrate.sh
@@ -26,18 +26,21 @@ fi
 echo $ARGS
 npm run migrate:$CONTRACT_ENV $ARGS
 
-APP_DEST="../app/src/contracts/$CONTRACT_ENV"
-# @woke/contracts package
-PKG_DEST="../contracts/$CONTRACT_ENV"
+BUILD_DIR="./build/contracts/artifacts"
+APP_DEST="app/src/contracts/$CONTRACT_ENV"
+PKG_DEST="contracts/$CONTRACT_ENV"
 mkdir -p "$APP_DEST"
 mkdir -p "$PKG_DEST"
 
-BUILD_DIR="./build/contracts/artifacts"
-echo "Copying contracts $APP_DEST ..."
-cp "$BUILD_DIR/TwitterOracleMock.json" "$APP_DEST"
-cp "$BUILD_DIR/WokeToken.json" "$APP_DEST"
-cp "$BUILD_DIR/UserRegistry.json" "$APP_DEST"
+echo "Adding contracts to app $APP_DEST ..."
+cp $BUILD_DIR/{TwitterOracleMock,WokeToken,UserRegistry}.json "../$APP_DEST"
 
-cp "$BUILD_DIR/TwitterOracleMock.json" "$PKG_DEST"
-cp "$BUILD_DIR/WokeToken.json" "$PKG_DEST"
-cp "$BUILD_DIR/UserRegistry.json" "$PKG_DEST"
+echo "Adding contracts to lib package $PKG_DEST ..."
+cp $BUILD_DIR/{TwitterOracleMock,WokeToken,UserRegistry}.json "../$PKG_DEST"
+
+CLONE=true
+PACKAGES_PATH="$HOME/Repositories/jgitgud/woke-dapp/@woke"
+if $CLONE; then
+	echo "Cloning artifacts into $PACKAGES_PATH ..."
+	cp $BUILD_DIR/{TwitterOracleMock,WokeToken,UserRegistry}.json "$PACKAGES_PATH/$PKG_DEST"
+fi
