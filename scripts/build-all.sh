c0c786a02e99bbca6d6b2d56853ad062b4bae744
Jack <jackson.virgo1@gmail.com>
2020-10-17 12:52:41 +1030

    scripts:wip: Consolidated docker scripts

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -2,34 +2,74 @@
 set -ex
 
 push=false
-while getopts i:p flag
+build=false
+
+while getopts i:pb flag
 do
-    case "${flag}" in
-        i) image=${OPTARG};;
-	p) push=true;;
-    esac
+	case "${flag}" in
+		i) module=${OPTARG};;
+		p) push=true;;
+		b) build=true;;
+	esac
 done
 
-echo "Image: $image";
+echo "Image: $module";
 echo "Push: $push";
+echo "Build: $build";
 
-# 1. List of images
-
-# 2. If none provided, build all
-
-# 3. If -p set, push to docker
-
-
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PARENT_DIR=$(basename "${PWD%/*}")
 CURRENT_DIR="${PWD##*/}"
-IMAGE_NAME="$PARENT_DIR/$CURRENT_DIR"
-TAG="${1}"
+#DOCKER_DIR="${SCRIPT_DIR}/../docker"
+DOCKER_DIR="docker"
 
-REGISTRY="hub.docker.com"
+cd $SCRIPT_DIR
+cd ..
 
-docker build -t ${REGISTRY}/${IMAGE_NAME}:${TAG} -t ${REGISTRY}/${IMAGE_NAME}:latest .
-docker push ${REGISTRY}/${IMAGE_NAME}
+REGISTRY="wokenetwork"
+IMAGE_NAME="woke"
+build() {
+	TAG=$1
+	# OR using local tags (not doing this for the moment, will need for
+	# seperating staging images from production images)
+	# docker build -t ${REGISTRY}/${IMAGE_NAME}:${TAG} -t ${REGISTRY}/${IMAGE_NAME}:latest .
 
+	# Store all the images under the same name as different tags (free lol)
+	docker build -f $DOCKER_DIR/${image}.Dockerfile -t ${REGISTRY}/${IMAGE_NAME}:${TAG} .
+}
 
-docker build -f oracle.Dockerfile -t jvindustries/woke:oracle .
-docker push jvindustries/woke:notifier
+push() {
+	TAG=$1
+	docker push ${REGISTRY}/${IMAGE_NAME}:${TAG}
+}
+
+single_image() {
+	image=$1
+	if ${build} = true; then
+		build $image
+	fi
+	if ${push} = true; then
+		push $image
+	fi
+}
+
+all_images() {
+	echo $DOCKER_DIR
+	for dockerfile in $DOCKER_DIR/*.Dockerfile; do
+		echo $dockerfile
+		# extract module name
+		M=$(basename $dockerfile .Dockerfile)
+		single_image $M
+	done
+}
+
+# If image name provided
+if [ -z ${module} ]; then
+	all_images
+else
+	if ls $DOCKER_DIR | grep -x -q "${module}.Dockerfile" ; then
+		single_image ${module}
+	else
+		echo "Docker image '${image}' does not exist"
+	fi
+fi
