cf206ca3ea6ff3efe453e9fda9f02a3ba17e05eb
Jack <jackson.virgo1@gmail.com>
2020-10-28 15:08:36 +1030

    app: update app ver

--- @woke/app/src/containers/root.js
+++ @woke/app/src/containers/root.js
@@ -21,7 +21,7 @@ import HedgehogWallet from '../lib/wallet/wallet';
 
 // Config
 const wallet = new HedgehogWallet();
-const appVersion = '0.4.1';
+const appVersion = '0.4.2';
 
 export default function RootContainer(props) {
 	const hedgehog = useHedgehog(wallet);
