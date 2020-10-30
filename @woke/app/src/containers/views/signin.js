b552e7747e84e458b577bea95026476045e3a82a
Jack <jackson.virgo1@gmail.com>
2020-10-30 10:30:38 +1030

    app: Update sign in text and manifest

--- @woke/app/src/containers/views/signin.js
+++ @woke/app/src/containers/views/signin.js
@@ -7,64 +7,73 @@ import BodyStandard from '../../components/text/body-standard';
 
 import { makeStyles } from '@material-ui/styles';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
 	title: {
-			fontSize: '8rem',
-			lineHeight: '8.5rem',
+		fontSize: '8rem',
+		lineHeight: '8.5rem',
 		[theme.breakpoints.down('sm')]: {
 			fontSize: '4rem',
 			lineHeight: '4.5rem',
 			marginBottom: '20%',
 		},
-	}
+	},
 }));
 
-const SHL = (props) => BodyStandard({ component: 'span', color: 'secondary', ...props});
-const Message = () => 
- <BodyStandard order={2}
+const SHL = (props) => BodyStandard({ component: 'span', color: 'secondary', ...props });
+const SHL_p = (props) => BodyStandard({ component: 'span', color: 'primary', ...props });
+
+const Message = () => (
+	<BodyStandard
+		order={2}
 		styles={{
 			paddingLeft: '0',
 			paddingRight: '0',
 			small: {
 				paddingLeft: '10%',
 				paddingRight: '10%',
-			}
+			},
 		}}
 	>
-	 Sign in with <SHL>twitter</SHL> to claim your enlightenment bonus and spread the wokeness.
-	</BodyStandard>;
+		Sign in with <SHL_p>twitter</SHL_p> to get your <SHL>$WOKENS</SHL>.
+	</BodyStandard>
+);
 
 export default function SigninView(props) {
 	const { triggerSignIn } = props;
 	const classes = useStyles();
 
-	return (<>
-		<FlexColumn styles={{
-			alignSelf: 'center',
-			marginBottom: '10%',
-			small: {
-				alignSelf: 'flex-start',
-				marginTop: '18%',
-			},
-			justifyContent: 'space-evenly'
-		}}>
-			<Typography variant="h1" className={classes.title} align="center" gutterBottom>
-				WOKE NETWORK
-			</Typography>
-
-			<Typography variant="h4" align="center" gutterBottom>
-				Share social impact.
-			</Typography>
-			<ButtonGroup reverse
-				message='Sign in with twitter to claim your enlightenment bonus and spread the wokeness.'
-				Message={Message}
-				buttonProps={{
-					onClick: triggerSignIn,
-					text: 'sign in',
-					color: 'primary',
-					iconLeft: <TwitterIcon/>,
+	return (
+		<>
+			<FlexColumn
+				styles={{
+					alignSelf: 'center',
+					marginBottom: '10%',
+					small: {
+						alignSelf: 'flex-start',
+						marginTop: '18%',
+					},
+					justifyContent: 'space-evenly',
 				}}
-			/>
-		</FlexColumn>
-	</>);
+			>
+				<Typography variant="h1" className={classes.title} align="center" gutterBottom>
+					WOKE NETWORK
+				</Typography>
+
+				<Typography variant="h4" align="center" gutterBottom>
+					Your clout power-up 🍄
+				</Typography>
+				<ButtonGroup
+					reverse
+					message="Sign in with twitter to get your Wokens."
+					Message={Message}
+					buttonProps={{
+						onClick: triggerSignIn,
+						text: 'sign in',
+						color: 'primary',
+						iconLeft: <TwitterIcon />,
+					}}
+				/>
+			</FlexColumn>
+		</>
+	);
 }
